07fc664485dc337e8233e17f963f3993828b14ce
政 <zheng@bogon.dhcp>
2017-06-28 22:34:11 +0800

    init

---
+++ WeChat/WeChat/Tabbar/ZZTabBarController.swift
@@ -0,0 +1,28 @@
+//
+//  ZZTabBarController.swift
+//  WeChat
+//
+//  Created by 政 on 2017/6/28.
+//  Copyright © 2017年 政. All rights reserved.
+//
+
+import UIKit
+
+class ZZTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    func initViewControllers() {
+        let titleArray = ["微信","通讯录","发现","我"]
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
